7d66abf4863d25ccbaf112bff10713f136e20e5f
Mark Delk <jethrodaniel@gmail.com>
2020-08-03 23:23:13 -0500

    document Odyssey::Formula

--- lib/odyssey/formulas/formula.rb
+++ lib/odyssey/formulas/formula.rb
@@ -1,32 +1,41 @@
-class Odyssey::Formula
-  # text will be a Hash like so:
-  # data = {
-  #   'raw' => String,
-  #   'words' => Array,
-  #   'sentences' => Array,
-  #   'syllables' => Array
-  # }
-  #
-  # stats will be a Hash like so:
-  # {
-  #   'string_length' => Fixnum,
-  #   'letter_count' => Fixnum,
-  #   'syllable_count' => Fixnum,
-  #   'word_count' => Fixnum,
-  #   'sentence_count' => Fixnum,
-  #   'average_words_per_sentence' => Float,
-  #   'average_syllables_per_word' => Float
-  # }
+module Odyssey
+  # A formula describes how a specifc readability metric is computed.
+  class Formula
+    # Compute a score for this formula.
+    #
+    # @param text [Hash]
+    # @option text [String] 'raw' the input string
+    # @option text [Array<String>] 'words'
+    # @option text [Array<String>] 'sentences'
+    # @option text [Array<String>] 'syllables'
+    #
+    # @param stats [Hash]
+    # @option stats [String] 'string_length'
+    # @option stats [String] 'letter_count'
+    # @option stats [String] 'syllable_count'
+    # @option stats [String] 'syllable_count'
+    # @option stats [String] 'word_count'
+    # @option stats [String] 'sentence_count'
+    # @option stats [String] 'average_words_per_sentence'
+    # @option stats [String] 'average_syllables_per_word'
+    #
+    # @return [Number]
+    def score(text, stats)
+      0
+    end
 
-  def score(text, stats)
-    0
-  end
+    # {score}, but per sentence.
+    # @params (see #score)
+    # see score
+    def score_by_sentence(text, stats_split)
+      0
+    end
 
-  def score_by_sentence(text, stats_split)
-    0
-  end
-
-  def name
-    'Generic'
+    # Name of this formula.
+    #
+    # @return [String]
+    def name
+      'Generic'
+    end
   end
 end
